fa350cb75a7a5907599cecea29b9668bcfe14eeb
Kozo Nishida <kozo.nishida@gmail.com>
2019-09-04 18:26:05 +0900

    Create bridgedbpy.py

---
+++ bridgedbpy.py
@@ -0,0 +1,16 @@
+import requests
+import re
+
+__author__ = 'Kozo Nishida'
+__email__ = 'knishida@riken.jp'
+__version__ = '0.0.1'
+__license__ = 'MIT'
+
+API_BASE = 'https://webservice.bridgedb.org/'
+
+def hmdb2kegg(hmdb_id):
+    r = requests.get(API_BASE + 'Human/xrefs/Ch/' + identifier + '?dataSource=Ck')
+    if r.status_code == 200:
+        return re.findall('C[0-9]{5}', r.content)
+    else:
+        print("Request failed for " + hmdb_id)
