2be0b31ceeab098ad51769f45aa1b7d8d97178b0
Kozo Nishida <kozo.nishida@gmail.com>
2021-08-31 13:11:11 +0900

    Update get_full_name.py

--- bridgedbpy/get_full_name.py
+++ bridgedbpy/get_full_name.py
@@ -1,1 +1,9 @@
+import sys
+import pandas
 
+def get_full_name(syscode):
+    df = pandas.read_csv("https://github.com/bridgedb/datasources/raw/main/datasources.tsv", sep="\t")
+    if not (df[1] == syscode).any():
+        sys.exit("You must provide a system code")
+    else:
+        return list(df[df[1] == syscode][0])[0]
