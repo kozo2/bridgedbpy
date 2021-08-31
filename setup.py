cf2d7ab112db35ba8f548c2bb3a01b6e41979082
Kozo Nishida <kozo.nishida@gmail.com>
2021-08-31 23:18:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/kozo2/bridgedbpy',
-    license='MIT License',
+    license='AGPL-3',
     keywords=['wikipathways', 'bioinformatics'],
     packages=setuptools.find_packages(),
     include_package_data=True,
