58ffc0063f8d243c668763380148bf30e838fa09
Kozo Nishida <kozo.nishida@gmail.com>
2021-08-31 21:57:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,38 @@
-from setuptools import setup
+import setuptools
 
-__version__ = '0.0.1'
+# Nasty but effective way to set __version__
+__version__=None
+exec(open("bridgedbpy/_version.py").read())
 
-with open("README.md") as f:
-    long_description = f.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(
+setuptools.setup(
     name='bridgedbpy',
     version=__version__,
-    url='https://github.com/ecell/bridgedbpy',
-    license='MIT',
-    py_modules=['bridgedbpy'],
-    python_requires='>=3.7',
-    author='Kozo Nishida',
-    author_email='kozo.nishida@gmail.com',
-    install_requires=['requests'],
-    description='Python wrapper for the BridgeDB webservices',
+    author="Kozo Nishida",
+    author_email="kozo.nishida@gmail.com",
+    maintainer="Kozo Nishida",
+    maintainer_email="kozo.nishida@gmail.com",
+    description="Code for using BridgeDb identifier mapping framework from within Python",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    classifiers=['License :: OSI Approved :: MIT License',]
+    url='https://github.com/kozo2/bridgedbpy',
+    license='MIT License',
+    keywords=['wikipathways', 'bioinformatics'],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=['requests', 'pandas'],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics'
+    ],
+    python_requires='>=3.7',
+    test_suite='tests',
 )
